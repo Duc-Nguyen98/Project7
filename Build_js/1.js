6d5df0662cce64b75a2f9e276f73179aab5ae714
= <=>
2019-07-18 11:29:04 +0700

    commit to repository

---
+++ Build_js/1.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded",function(){
+	//code o day
+	//calling Brand Crousel
+		$(".main-banner").owlCarousel({
+			loop:true,
+			margin:5,
+			responsiveClass:true,
+			slideSpeed : 2000,
+			smartSpeed:2500,
+			nav: true,
+			autoplay: true,
+			autoplayTimeout:5000,
+			responsiveRefreshRate : 200,
+			rewind:true,
+			responsive:{
+				0:{
+					items:1,
+					nav:false,
+					navText:["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"]
+				},
+				600:{
+					items:1,
+					nav:false,
+					navText:["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"]
+				},
+				1000:{
+					items:1,
+					nav:false,
+					navText:["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+					loop:true
+				}
+			}
+		});		
+},false)
